9d2d2dccf0064e919aadbeaaff95580741556602
sarbudeen <sarbudeenmca@gmail.com>
2024-01-10 18:08:40 +0400

    form started

--- src/components/Leads.jsx
+++ src/components/Leads.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ViewLeads from './views/ViewLeads'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import AddLead from './forms/AddLead'
 
 const Leads = () => {
   return (
     <>
       <div className='title-header'>
         <h1 className='main-title'>Leads</h1>
-        <button className='btn-add-new'>Add New</button>
+        <button className='btn-add-new'><FontAwesomeIcon icon={faPlus} /><span>Add New</span></button>
       </div>
       <ViewLeads />
+      <AddLead />
     </>
   )
 }
--- src/components/forms/AddLead.jsx
+++ src/components/forms/AddLead.jsx
@@ -1,10 +1,23 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const AddLead = () => {
     return (
-        <>
-            
-        </>
+        <aside className='form'>
+            <div className="title-header">
+                <h1 className="main-title">Add New Lead</h1>
+                <button className='form-close'><FontAwesomeIcon icon={faTimes} /></button>
+            </div>
+            <form action="#">
+                <div className="form-container">
+                    <div className="form-fields w-3/6">
+                        <label htmlFor="leadName" className="form-label"></label>
+                        <input type="text" className="form-input" id="leadName" />
+                    </div>
+                </div>
+            </form>
+        </aside>
     )
 }
 
