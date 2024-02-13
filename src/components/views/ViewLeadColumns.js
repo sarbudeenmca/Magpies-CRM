4bb7abce0732e7c49250ceb7e25d9dbbbb5c9ce8
Sarbudeen <sarbudeenmca@gmail.com>
2024-02-13 23:38:08 +0400

    React Table Working

---
+++ src/components/views/ViewLeadColumns.js
@@ -0,0 +1,46 @@
+export const COLUMNS = [
+    {
+        header: 'Id',
+        accessor: 'id'
+    },
+    {
+        header: 'Lead Name',
+        accessor: 'laed_name'
+    },
+    {
+        header: 'Company Name',
+        accessor: 'company_name'
+    },
+    {
+        header: 'Phone',
+        accessor: 'phone_number'
+    },
+    {
+        header: 'Mobile',
+        accessor: 'mobile_number'
+    },
+    {
+        header: 'Email',
+        accessor: 'email_address'
+    },
+    {
+        header: 'City',
+        accessor: 'city'
+    },
+    {
+        header: 'Country',
+        accessor: 'country'
+    },
+    {
+        header: 'Lead Status',
+        accessor: 'laed_status'
+    },
+    {
+        header: 'Lead Source',
+        accessor: 'laed_source'
+    },
+    {
+        header: 'Description',
+        accessor: 'description'
+    }
+]
---
+++ src/components/views/ViewLeadsTable.js
@@ -0,0 +1,11 @@
+import { useTable } from 'react-table'
+import { COLUMN } from './ViewLeadColumns'
+import React from 'react'
+
+const ViewLeadsTable = () => {
+    return (
+        <div>ViewLeadsTable</div>
+    )
+}
+
+export default ViewLeadsTable
