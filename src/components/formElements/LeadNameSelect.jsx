0e668eb69a8c533fe8bd47f0a70ab1de12d5bba0
sarbudeen <sarbudeenmca@gmail.com>
2024-03-20 14:57:13 +0400

    React-Select dynamic data load finish

--- src/components/formElements/LeadNameSelect.jsx
+++ src/components/formElements/LeadNameSelect.jsx
@@ -1,46 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from '../../api/axios'
 import Select from 'react-select';
 
 const LeadNameSelect = () => {
     const [selectedLeadName, setSelectedLeadName] = useState('')
-    const [options, setOptions] = useState([])
-
-    const handleChangeLeadName = (selectedLeadName) => {
-        setSelectedLeadName(selectedLeadName)
-    }
-
+    const [leadNames, setLeadNames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     //Lead Name fetching
-    const fetchLeadNames = async (input) => {
-        try {
-            const response = await axios.get(`/api/leadnames`, {
-                params: { keyword: input }
-            })
-            const leadNames = response.data.leadNames.map(lead => ({
-                value: lead.id,
-                label: lead.lead_name
-            }))
-            return leadNames
-        } catch (error) {
-            console.error('Error fetching lead names:', error)
-            return []
+    useEffect(() => {
+        const fetchData = async (input) => {
+            try {
+                const response = await axios.get(`/api/leadnames`, {
+                    params: { keyword: input }
+                })
+                const leadNames = response.data.leadNames.map(lead => ({
+                    value: lead.id,
+                    label: lead.lead_name
+                }))
+                // return leadNames
+                setLeadNames(leadNames)
+            } catch (error) {
+                console.error('Error fetching lead names:', error)
+                return []
+            } finally {
+                setIsLoading(false);
+            }
         }
+
+        fetchData()
+    }, [])
+
+    const handleChangeLeadName = (selectedOption) => {
+        setSelectedLeadName(selectedOption)
     }
+
+    const loadOptions = (input) => {
+        return leadNames.filter(option =>
+            option.label.toLowerCase().includes(input.toLowerCase())
+        );
+    };
 
     return (
         <Select
+            className="react-form-select peer"
             id="leadName"
             name="leadName"
-            className="form-select peer"
             value={selectedLeadName}
             onChange={handleChangeLeadName}
-            loadOptions={fetchLeadNames}
-            onInputChange={(inputValue) => {
-                return fetchLeadNames(inputValue); // Ensure fetchLeadNames is called with the input value
-            }}
+            options={leadNames}
             isClearable={true}
-            placeholder="Type to search leads"
-            isSearchable
+            placeholder={isLoading ? "Loading..." : "Type to search leads"}
+            isSearchable={!isLoading}
+            isLoading={isLoading}
+            styles={{
+                control: (baseStyles) => ({
+                    ...baseStyles,
+                    background: 'transparent',
+                    border: 'none',
+                }),
+            }}
+            classNames={{
+                control: (state) =>
+                    state.isFocused ? 'border-red-600' : 'border-grey-300',
+            }}
         />
     )
 }
